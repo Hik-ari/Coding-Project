36e1f3f94d1ba7aed1fe29bf71b4db83eefc579c
Hikari <145105541+Hik-ari@users.noreply.github.com>
2023-09-17 09:57:50 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-dlkfgjdfjg;lj;ld
-KKKKK
+import pygame
+import sys
+
+pygame.init()
+
+WIDTH = 800
+HEIGHT = 600
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+run = True
+while run:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      sys.exit()
+pygame.quit()
