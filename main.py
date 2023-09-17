04071dccf94c1c24cdd019a20bb41ef932734680
Hikari <145105541+Hik-ari@users.noreply.github.com>
2023-09-17 10:03:34 +1000

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ pygame.init()
 
 WIDTH = 800
 HEIGHT = 600
+FPS = 60
+
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Platformer")
+clock = pygame.time.Clock()
 
 run = True
 while run:
+
+  clock.tick(FPS)
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       sys.exit()
